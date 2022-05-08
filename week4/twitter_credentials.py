f2ee61002f5ed4a53503e9ace154687194291f5c
JelleTasma <62698726+JelleTasma@users.noreply.github.com>
2022-05-08 20:54:53 +0200

    Add files via upload

---
+++ week4/twitter_credentials.py
@@ -0,0 +1,7 @@
+ACCESS_TOKEN = "2508230580-F6ukyAagNNT4wpO7XxB5Swg8u4b9V8iPMXhnZsp" 
+ACCESS_TOKEN_SECRET = "Ka6Z5nFolM2kRfNd9WrdeXIvycmzlD2Iilx6UDaaQBcmB" 
+CONSUMER_KEY = "ZEnb9XYlSX6YkRh07jvddgv2Z" 
+CONSUMER_SECRET = "q8WlbFr4kDrge2A7Oc5neoWnniLr32s58ATIK1xTVIgnGjgDoi"
+
+import pandas as pd
+df = pd.read_csv("births.csv")
